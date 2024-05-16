d43c8e3a1f7ccaedb3ee8921df715d362af41214
its-asif <asifiscoding@gmail.com>
2024-05-17 02:16:30 +0600

    [feat](User): UserController and userRoute updated

--- routeHandler/UserRoute.js
+++ routeHandler/UserRoute.js
@@ -6,8 +6,14 @@ const userController = require('../controllers/UserController');
 // Get all the users
 router.get('/', userController.getAllUsers);
 
+// Get a user by id
+router.get('/:id', userController.getUserById);
+
 // POST a user
 router.post('/', userController.createUser);
+
+// DELETE a user
+router.delete('/:id', userController.deleteUser);
 
 
 module.exports = router;
